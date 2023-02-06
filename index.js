cf2c983a1d7350c111d381f8c1b13657571fd472
SoNomad <nurmuhammad16203@gmail.com>
2023-02-06 18:58:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log('База подключена');
+  })
+  .catch((e) => {
+    console.log(e.toString());
+  });
+
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log(err.toString());
+  }
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
