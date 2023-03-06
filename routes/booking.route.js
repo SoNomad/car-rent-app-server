7fed19bf079662235e7cd88f4abac8746867d865
SoNomad <nurmuhammad16203@gmail.com>
2023-03-07 02:29:41 +0300

    Добавлен отдельный контроллер для проверки даты.

--- routes/booking.route.js
+++ routes/booking.route.js
@@ -4,6 +4,7 @@ const { bookingController } = require('../controllers/booking.controller');
 const router = Router();
 router.get('/booking', bookingController.getBooks);
 router.post('/booking', bookingController.addBook);
+router.post('/booking/check', bookingController.checkReserved);
 router.delete('/booking/:id', bookingController.deleteBook);
 // router.patch('/car/:id', carsController.editCar);
 
